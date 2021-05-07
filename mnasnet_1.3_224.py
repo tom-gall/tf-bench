32e815519dc69050a6596c489ae4cf712ea6c241
Tom Gall <tom.gall@linaro.org>
2021-05-07 02:28:53 +0000

    Fix download of model zoo reference model add support for get_cpu_count()
    Signed-off-by: Tom Gall <tom.gall@linaro.org>

--- mnasnet_1.3_224.py
+++ mnasnet_1.3_224.py
@@ -11,13 +11,23 @@ import numpy as np
 
 from PIL import Image
 from tflite_runtime.interpreter import Interpreter
-from util import set_input_tensor, classify_image, load_test_image, download_model_zoo,parse_options, get_device_arch, get_device_attributes, get_device_type, parse_options
+from util import set_input_tensor, classify_image, load_test_image, download_model_zoo,parse_options, get_device_arch, get_device_attributes, get_device_type, parse_options, get_cpu_count
 
 model_dir = './mnasnet_1.3_224/'
 model_name ='mnasnet_1.3_224.tflite'
 repeat=10
 
-interpreter = Interpreter(tflite_model_file)
+model_dir = download_model_zoo(model_dir, model_name)
+tflite_model_file = os.path.join(model_dir, model_name)
+tflite_model_buf = open(tflite_model_file, "rb").read()
+try:
+    import tflite
+    tflite_model = tflite.Model.GetRootAsModel(tflite_model_buf, 0)
+except AttributeError:
+    import tflite.Model
+    tflite_model = tflite.Model.Model.GetRootAsModel(tflite_model_buf, 0)
+
+interpreter = Interpreter(tflite_model_file, num_threads=get_cpu_count())
 interpreter.allocate_tensors()
 
 _, height, width, _ = interpreter.get_input_details()[0]['shape']
